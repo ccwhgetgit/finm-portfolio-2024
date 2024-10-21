8eee389dc7133747dfa184ef7eae40d7cca8667d
ccwhgetgit <colinchanwh@gmail.com>
2024-10-21 07:56:01 -0500

    updated folder (synced)

--- cmds/portfolio_management_helper.py
+++ cmds/portfolio_management_helper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from arch import arch_model
 import math
 import datetime
 pd.options.display.float_format = "{:,.4f}".format
@@ -1293,7 +1294,6 @@ def calc_iterative_regression(
     Returns:
     pd.DataFrame: Summary statistics for each asset regression.
     """
-    raise Exception("Function not available - needs testing prior to use")
     multiple_y = multiple_y.copy()
     X = X.copy()
 
@@ -1616,21 +1616,63 @@ def create_portfolio(
     return port_returns
 
 
+def calc_ewma_volatility(
+        excess_returns: pd.Series,
+        theta : float = 0.94,
+        initial_vol : float = .2 / np.sqrt(252)
+    ) -> pd.Series:
+    var_t0 = initial_vol ** 2
+    ewma_var = [var_t0]
+    for i in range(len(excess_returns.index)):
+        new_ewma_var = ewma_var[-1] * theta + (excess_returns.iloc[i] ** 2) * (1 - theta)
+        ewma_var.append(new_ewma_var)
+    ewma_var.pop(0) # Remove var_t0
+    ewma_vol = [np.sqrt(v) for v in ewma_var]
+    return pd.Series(ewma_vol, index=excess_returns.index)
+
+
+def calc_garch_volatility(
+        excess_returs: pd.Series,
+        p: int = 1,
+        q: int = 1
+    ) -> pd.Series:
+    model = arch_model(excess_returs, vol='Garch', p=p, q=q)
+    fitted_model = model.fit(disp='off')
+    fitted_values = fitted_model.conditional_volatility
+    return fitted_values
+
+
+def calc_garch_volatility(
+        excess_returs: pd.Series,
+        p: int = 1,
+        q: int = 1
+    ):
+    model = arch_model(excess_returs, vol='Garch', p=p, q=q)
+    fitted_model = model.fit(disp='off')
+    fitted_values = fitted_model.conditional_volatility
+    return pd.Series(fitted_values, index=excess_returs.index)
+
+
 def calc_var_cvar_summary(
     returns: Union[pd.Series, pd.DataFrame],
     quantile: Union[None, float] = .05,
     window: Union[None, str] = None,
     return_hit_ratio: bool = False,
+    filter_first_hit_ratio_date: Union[None, str, datetime.date] = None,
     return_stats: Union[str, list] = ['Returns', 'VaR', 'CVaR', 'Vol'],
     full_time_sample: bool = False,
     z_score: float = None,
     shift: int = 1,
     normal_vol_formula: bool = False,
+    ewma_theta : float = .94,
+    ewma_initial_vol : float = .2 / np.sqrt(252),
+    garch_p: int = 1,
+    garch_q: int = 1,
     keep_columns: Union[list, str] = None,
     drop_columns: Union[list, str] = None,
     keep_indexes: Union[list, str] = None,
     drop_indexes: Union[list, str] = None,
-    drop_before_keep: bool = False
+    drop_before_keep: bool = False,
 ):
     """
     Calculates a summary of VaR (Value at Risk) and CVaR (Conditional VaR) for the provided returns.
@@ -1676,20 +1718,30 @@ def calc_var_cvar_summary(
     else:
         summary[f'Expanding {window:.0f} Volatility'] = np.sqrt((returns ** 2).expanding(window).mean())
         summary[f'Rolling {window:.0f} Volatility'] = np.sqrt((returns ** 2).rolling(window).mean())
+    summary[f'EWMA {ewma_theta:.2f} Volatility'] = calc_ewma_volatility(returns, theta=ewma_theta, initial_vol=ewma_initial_vol)
+    summary[f'GARCH({garch_p:.0f}, {garch_q:.0f}) Volatility'] = calc_garch_volatility(returns, p=garch_p, q=garch_q)
 
     z_score = norm.ppf(quantile) if z_score is None else z_score
     summary[f'Expanding {window:.0f} Parametric VaR ({quantile:.2%})'] = summary[f'Expanding {window:.0f} Volatility'] * z_score
     summary[f'Rolling {window:.0f} Parametric VaR ({quantile:.2%})'] = summary[f'Rolling {window:.0f} Volatility'] * z_score
+    summary[f'EWMA {ewma_theta:.2f} Parametric VaR ({quantile:.2%})'] = summary[f'EWMA {ewma_theta:.2f} Volatility'] * z_score
+    summary[f'GARCH({garch_p:.0f}, {garch_q:.0f}) Parametric VaR ({quantile:.2%})'] = summary[f'GARCH({garch_p:.0f}, {garch_q:.0f}) Volatility'] * z_score
 
     if return_hit_ratio:
         shift_stats = [
             f'Expanding {window:.0f} Historical VaR ({quantile:.2%})',
             f'Rolling {window:.0f} Historical VaR ({quantile:.2%})',
             f'Expanding {window:.0f} Parametric VaR ({quantile:.2%})',
-            f'Rolling {window:.0f} Parametric VaR ({quantile:.2%})'
+            f'Rolling {window:.0f} Parametric VaR ({quantile:.2%})',
+            f'EWMA {ewma_theta:.2f} Parametric VaR ({quantile:.2%})',
+            f'GARCH({garch_p:.0f}, {garch_q:.0f}) Parametric VaR ({quantile:.2%})'
         ]
         summary_shift = summary.copy()
         summary_shift[shift_stats] = summary_shift[shift_stats].shift()
+        if filter_first_hit_ratio_date:
+            if isinstance(filter_first_hit_ratio_date, (datetime.date, datetime.datetime)):
+                filter_first_hit_ratio_date = filter_first_hit_ratio_date.strftime("%Y-%m-%d")
+            summary_shift = summary_shift.loc[filter_first_hit_ratio_date:]
         summary_shift = summary_shift.dropna(axis=0)
         summary_shift[shift_stats] = summary_shift[shift_stats].apply(lambda x: (x - summary_shift['Returns']) > 0)
         hit_ratio = pd.DataFrame(summary_shift[shift_stats].mean(), columns=['Hit Ratio'])
@@ -1697,6 +1749,7 @@ def calc_var_cvar_summary(
         hit_ratio['Hit Ratio Absolute Error'] = abs(hit_ratio['Hit Ratio Error'])
         hit_ratio = hit_ratio.sort_values('Hit Ratio Absolute Error')
         print(hit_ratio)
+        
         return filter_columns_and_indexes(
             hit_ratio,
             keep_columns=keep_columns,
@@ -1711,13 +1764,15 @@ def calc_var_cvar_summary(
     summary[f'Rolling {window:.0f} Historical CVaR ({quantile:.2%})'] = returns.rolling(window).apply(lambda x: x[x < x.quantile(quantile)].mean())
     summary[f'Expanding {window:.0f} Parametrical CVaR ({quantile:.2%})'] = - norm.pdf(z_score) / quantile * summary[f'Expanding {window:.0f} Volatility']
     summary[f'Rolling {window:.0f} Parametrical CVaR ({quantile:.2%})'] = - norm.pdf(z_score) / quantile * summary[f'Rolling {window:.0f} Volatility']
+    summary[f'EWMA {ewma_theta:.2f} Parametrical CVaR ({quantile:.2%})'] = - norm.pdf(z_score) / quantile * summary[f'EWMA {ewma_theta:.2f} Volatility']
+    summary[f'GARCH({garch_p:.0f}, {garch_q:.0f}) Parametrical CVaR ({quantile:.2%})'] = - norm.pdf(z_score) / quantile * summary[f'GARCH({garch_p:.0f}, {garch_q:.0f}) Volatility']
 
     if shift > 0:
         shift_columns = [c for c in summary.columns if not bool(re.search("returns", c))]
         summary[shift_columns] = summary[shift_columns].shift(shift)
         print(f'VaR and CVaR are given shifted by {shift:0f} period(s).')
     else:
-        print('VaR and CVaR are given in-sample, as specified by the formula.')
+        print('VaR and CVaR are given in-sample.')
 
     if full_time_sample:
         summary = summary.loc[:, lambda df: [c for c in df.columns if bool(re.search('expanding', c.lower()))]]
@@ -2064,6 +2119,7 @@ def calc_dynamic_carry_trade(
     Returns:
     pd.DataFrame: Summary of the carry trade strategy statistics or premium series.
     """
+    raise Exception("Function not available - needs testing prior to use")
     if annual_factor is None:
         print("Regression assumes 'annual_factor' equals to 12 since it was not provided")
         annual_factor = 12
